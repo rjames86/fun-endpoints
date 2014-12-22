b2d264c845b217656ec755d74741694d4c83728f
Ryan M <rjames86@gmail.com>
2014-12-21 17:16:35 -0800

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -5,15 +5,15 @@ from config import config
 bootstrap = Bootstrap()
 
 def create_app(config_name):
-  app = Flask(__name__)
-  app.config.from_object(config[config_name])
-  config[config_name].init_app(app)
+      app = Flask(__name__)
+      app.config.from_object(config[config_name])
+      config[config_name].init_app(app)
 
-  bootstrap.init_app(app)
+      bootstrap.init_app(app)
 
-  from .main import main as main_blueprint
-  from .urls import urls as url_blueprint
-  app.register_blueprint(main_blueprint, url_prefix='/')
-  app.register_blueprint(url_blueprint, url_prefix='/urls')
+      from .main import main as main_blueprint
+      from .urls import urls as url_blueprint
+      app.register_blueprint(main_blueprint, url_prefix='/')
+      app.register_blueprint(url_blueprint, url_prefix='/urls')
 
-  return app
+      return app
