ff7ac91a742bef56cecd573b62ea95f696f2fb96
rjames86@gmail.com <rjames86@gmail.com>
2014-12-23 21:48:10 +0000

    stupid mistakes

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ class HerokuConfig(ProductionConfig):
         import logging
         from logging import StreamHandler
         file_handler = StreamHandler()
-        file_handler.setLevel(logging.Error)
+        file_handler.setLevel(logging.ERROR)
         app.logger.addHandler(file_handler)
 
 
