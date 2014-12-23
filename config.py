96fd98b6b24765c11605d9d75acf794a93b0ec72
rjames86@gmail.com <rjames86@gmail.com>
2014-12-23 21:42:48 +0000

    logging isn't working for some reason

--- config.py
+++ config.py
@@ -39,13 +39,14 @@ class ProductionConfig(Config):
 class HerokuConfig(ProductionConfig):
     @classmethod
     def init_app(cls, app):
+        print "Using Heroku config"
         ProductionConfig.init_app(app)
 
         # log to stderr
         import logging
         from logging import StreamHandler
         file_handler = StreamHandler()
-        file_handler.setLevel(logging.ERROR)
+        file_handler.setLevel(logging.INFO)
         app.logger.addHandler(file_handler)
 
 
