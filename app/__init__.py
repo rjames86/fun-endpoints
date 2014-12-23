c756e69b63033839601d246986fb4c044b524805
rjames86@gmail.com <rjames86@gmail.com>
2014-12-23 19:21:12 +0000

    add proxiedrequest to get real IP

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask.ext.bootstrap import Bootstrap
 from config import config
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from lib.webutils import ProxiedRequest
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -13,6 +14,7 @@ login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.request_class = ProxiedRequest
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
