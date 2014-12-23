59bc284bd3b9c0c219e7448125051851dbce633f
rjames86@gmail.com <rjames86@gmail.com>
2014-12-23 19:06:18 +0000

    add ip endpoint

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,15 @@
+from flask import (
+    jsonify,
+    request
+)
 from . import main
 
 
 @main.route('/')
 def index():
     return "Hello World."
+
+
+@main.route("/ip", methods=["GET"])
+def get_my_ip():
+    return jsonify({'ip': request.remote_addr}), 200
