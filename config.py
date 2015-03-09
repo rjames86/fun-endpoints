db6477df753be4d71b3d5d3c77fbab6353cb0d20
Ryan M <rjames86@gmail.com>
2015-03-08 23:33:09 -0700

    Revert "INFO logging"
    This reverts commit 6227092a354994696e1379d2a613212bf077942a.

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ class HerokuConfig(ProductionConfig):
         import logging
         from logging import StreamHandler
         file_handler = StreamHandler()
-        file_handler.setLevel(logging.INFO)
+        file_handler.setLevel(logging.DEBUG)
         app.logger.addHandler(file_handler)
 
 
