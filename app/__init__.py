2317f79d7277d31f97516711cc84383cbc691ae5
rjames86@gmail.com <rjames86@gmail.com>
2015-03-09 06:17:33 +0000

    removed db

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,6 @@ def create_app(config_name):
     config[config_name].init_app(app)
     app.request_class = ProxiedRequest
 
-    db.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
 
