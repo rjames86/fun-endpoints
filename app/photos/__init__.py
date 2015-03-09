77c2746ad0dcc02fd23b62a03895cc6bddd8032f
rjames86@gmail.com <rjames86@gmail.com>
2015-03-09 03:34:09 +0000

    removing dropbox related imports

--- app/photos/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-photos = Blueprint('photos', __name__)
-
-from . import views
--- app/tp/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-tp = Blueprint('tp', __name__)
-
-from . import views
