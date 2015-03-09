6227092a354994696e1379d2a613212bf077942a
rjames86@gmail.com <rjames86@gmail.com>
2015-03-09 03:27:56 +0000

    INFO logging

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ class HerokuConfig(ProductionConfig):
         import logging
         from logging import StreamHandler
         file_handler = StreamHandler()
-        file_handler.setLevel(logging.DEBUG)
+        file_handler.setLevel(logging.INFO)
         app.logger.addHandler(file_handler)
 
 
