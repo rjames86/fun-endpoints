a8f91f1695d6963e2b8fc6ed56cf700664be6e74
Ryan M <rjames86@gmail.com>
2015-03-12 21:30:35 -0700

    forgot to inject permissions

--- app/apartment/__init__.py
+++ app/apartment/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from ..lib.decorators import gen_route_decorator
-from ..models import ApartmentUnits
+from ..models import ApartmentUnits, Permission
 
 apartment = Blueprint('apartment', __name__)
 route = gen_route_decorator(apartment)
@@ -10,4 +10,7 @@ from . import views
 
 @apartment.app_context_processor
 def inject_apartments():
-    return dict(ApartmentUnits=ApartmentUnits)
+    return dict(
+        ApartmentUnits=ApartmentUnits,
+        Permission=Permission
+    )
