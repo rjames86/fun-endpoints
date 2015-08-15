6ac165d6c80e25b3de152239b139863e4020650f
Ryan M <rjames86@gmail.com>
2015-08-15 06:27:19 -0700

    fix

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Ryan M <apartment@ryanmo.co>'
+    FLASKY_MAIL_SENDER = os.environ.get('MAIL_SENDER')
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SSL_DISABLE = True
 
