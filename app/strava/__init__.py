a5c498ef89e58541d4ab55ac0e69138da95e7fb1
Ryan M <rjames86@gmail.com>
2015-12-31 13:06:39 -0800

    ugh

--- app/strava/__init__.py
+++ app/strava/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from ..lib.decorators import gen_route_decorator, json_route_decorator
 
 strava = Blueprint('strava', __name__)
-route = gen_route_decorator(main)
-as_json = json_route_decorator(main)
+route = gen_route_decorator(strava)
+as_json = json_route_decorator(strava)
 
 from . import views
