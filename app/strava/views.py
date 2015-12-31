4ab90e57a15c29cf23e1a22164b5745d80efb682
Ryan M <rjames86@gmail.com>
2015-12-31 13:24:49 -0800

    another fix

--- app/strava/views.py
+++ app/strava/views.py
@@ -15,7 +15,7 @@ from ..models import Strava
 
 @strava.before_app_request
 def before_request():
-    if not session.get('strava_token'):
+    if not session.get('strava_token') and 'authorize' not in request.endpoint:
         return redirect(url_for('strava.authorize'))
 
 @route('/')
