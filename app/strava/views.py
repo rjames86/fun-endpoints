8b9fbc6fbdecf022109acf621e41022dd911f14a
Ryan M <rjames86@gmail.com>
2016-01-13 18:46:42 -0800

    testing streaming templates

--- app/strava/views.py
+++ app/strava/views.py
@@ -8,6 +8,7 @@ from flask import (
     current_app,
     jsonify,
     g,
+    Response
 )
 from . import strava, route, as_json
 from ..lib.decorators import cached
@@ -18,6 +19,19 @@ import datetime
 
 from app.models.strava import Strava, CalendarInfo, AverageMileageChart
 
+import time
+def timer(method):
+
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+
+        print '%r (%r, %r) %2.2f sec' % \
+              (method.__name__, args, kw, te-ts)
+        return result
+
+    return timed
 
 @strava.before_app_request
 def before_request():
@@ -26,13 +40,33 @@ def before_request():
     if not session.get('strava_token'):
         return redirect(url_for('strava.authorize'))
 
+def stream_template(template_name, **context):
+    current_app.update_template_context(context)
+    t = current_app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    rv.enable_buffering(5)
+    return rv
+
 
 @route('/')
 def index():
     return redirect(url_for('strava.activity', activity_type='ride'))
 
 
+@route('/by_token/<token>')
+def by_token(token):
+    session['strava_token'] = token
+    return redirect(url_for('strava.activity', activity_type='ride'))
+
+
+@route('/reset_token')
+def reset_token():
+    del session['strava_token']
+    return redirect(url_for('strava.index'))
+
+
 @route('/<activity_type>')
+@timer
 def activity(activity_type):
     if activity_type == 'runs':
         activity_type = 'run'
@@ -41,12 +75,10 @@ def activity(activity_type):
     calendar_info = CalendarInfo.by_activities(strava.activities)
     mileage_chart = AverageMileageChart.by_activities(strava.activities)
 
-    return render_template('strava/index.html',
-                           activities=strava.activities,
-                           calendar_info=calendar_info,
-                           mileage_chart=mileage_chart)
-
-
+    return Response(stream_template('strava/index.html',
+                                    activities=strava.activities,
+                                    calendar_info=calendar_info,
+                                    mileage_chart=mileage_chart))
 
 @route('/auth/authorize')
 def authorize():
