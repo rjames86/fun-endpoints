0ba3f5a87b09e229c042b44e9b5d55cbc5a007fd
Ryan M <rjames86@gmail.com>
2016-01-22 10:21:49 -0800

    removing email address

--- config.py
+++ config.py
@@ -24,7 +24,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    FLASKY_ADMIN = 'rjames86@gmail.com'
     STRAVA_CLIENT_SECRET = 'f807fb8e15a246ff9f25de90bc2e1294fd51ab63'
     STRAVA_CLIENT_ID = 9426
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
